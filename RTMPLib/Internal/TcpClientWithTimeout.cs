1b81d58da444eec62d5642901f84e027e20ca055
Frederik Gelder <frederik.gelder@freenet.de>
2018-01-16 22:02:43 +0100

    updated tcpclientwithtimeout to work properly (i hope). also updated gitignore

--- RTMPLib/Internal/TcpClientWithTimeout.cs
+++ RTMPLib/Internal/TcpClientWithTimeout.cs
@@ -26,74 +26,77 @@ using System.Net;
 
 namespace RTMPLib.Internal
 {
-	public class TcpClientWithTimeout
-	{
-		public TcpClient InternalClient
-		{
-			get;
-			private set;
-		}
+    public class TcpClientWithTimeout
+    {
+        public TcpClient InternalClient
+        {
+            get;
+            private set;
+        }
 
-		public bool Connected
-		{
-			get;
-			private set;
-		}
+        private volatile bool connected = false;
+        public bool Connected
+        {
+            get { return connected; }
+            private set { connected = value; }
+        }
 
-		public IPEndPoint EndPoint
-		{
-			get;
-			private set;
-		}
-		protected Exception exception;
+        public IPEndPoint EndPoint
+        {
+            get;
+            private set;
+        }
+        protected Exception exception;
 
-		public TcpClientWithTimeout(IPEndPoint ipe)
-		{
-			EndPoint = ipe;
-		}
+        public TcpClientWithTimeout(IPEndPoint ipe)
+        {
+            EndPoint = ipe;
+        }
 
-		public void Connect(int timeoutMilliseconds = 2000)
-		{
-			Connected = false;
-			exception = null;
-			Thread connectorThread = new Thread(TryConnect);
-			connectorThread.IsBackground = true; // So that a failed connection attempt wont prevent the process from terminating while it does a long timeout
-			connectorThread.Start();
+        public void Connect(int timeoutMilliseconds = 2000)
+        {
+            Connected = false;
+            exception = null;
+            Thread connectorThread = new Thread(TryConnect);
+            connectorThread.IsBackground = true; // So that a failed connection attempt wont prevent the process from terminating while it does a long timeout
+            connectorThread.Start();
 
-			// wait for the thread to finish
-			connectorThread.Join(timeoutMilliseconds);
+            // wait for the thread to finish
+            connectorThread.Join(timeoutMilliseconds);
 
-			if (Connected == true)
-			{
-				// it succeeded
-			}
-			if (exception != null)
-			{
-				// it crashed
-				throw exception;
-			}
-			else
-			{
-				// it timed out
-				connectorThread.Abort();
-				string message = string.Format("TcpClient connection to {0}:{1} timed out", EndPoint.Address, EndPoint.Port);
-				throw new TimeoutException(message);
-			}
-		}
+            if(Connected == true)
+            {
+                // it succeeded
+                return;
+            }
+            if(exception != null)
+            {
+                // it crashed
+                throw exception;
+            }
+            else
+            {
+                // it timed out
+                connectorThread.Abort();
+                string message = string.Format("TcpClient connection to {0}:{1} timed out", EndPoint.Address, EndPoint.Port);
+                throw new TimeoutException(message);
+            }
+        }
 
-		protected void TryConnect()
-		{
-			try
-			{
-				InternalClient = new TcpClient(EndPoint);
-				// record that it succeeded, for the main thread to return to the caller
-				Connected = true;
-			}
-			catch (Exception ex)
-			{
-				// record the exception for the main thread to re-throw back to the calling code
-				exception = ex;
-			}
-		}
-	}
+        protected void TryConnect()
+        {
+            try
+            {
+                InternalClient = new TcpClient();
+                InternalClient.Connect(EndPoint);
+                // record that it succeeded, for the main thread to return to the caller
+                Connected = true;
+            }
+            catch(Exception ex)
+            {
+                // record the exception for the main thread to re-throw back to the calling code
+                exception = ex;
+            }
+        }
+    }
 }
